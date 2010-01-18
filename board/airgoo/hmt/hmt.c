93cc6df736c084ef86f73e561dd3d6feb523e63a
Peter Korsgaard <jacmet@sunsite.dk>
2010-01-19 00:16:07 +0100

    hmt: misc_init_r(): setup mifpcon + gpio as wince eboot does
    Fixes framebuffer and USB config.

--- board/airgoo/hmt/hmt.c
+++ board/airgoo/hmt/hmt.c
@@ -22,6 +22,7 @@
 #include <common.h>
 #include <s3c6400.h>
 #include <i2c.h>
+#include <asm/io.h>
 
 #define MAX8698_ONOFF1	0
 #define MAX8698_ONOFF2	1
@@ -57,6 +58,71 @@ int misc_init_r(void)
 	max8698_write(MAX8698_ONOFF1, 0x3e); /* enable LDO3+4 */
 	max8698_write(MAX8698_ONOFF2, 0xf1); /* enable LDO7+8 */
 	max8698_write(MAX8698_LDO9, 0x11); /* LDO9 = 3.3V */
+
+	writel(0,0x7410800c); /* panel */
+	writel(0x1801e, 0x7e00f900); /* usb */
+
+	writel(0x22221122, 0x7f008000); /* gpa */
+	writel(0xff, 0x7f008004);
+	writel(0xa0, 0x7f008008);
+
+	writel(0x02211122, 0x7f008020); /* gpb */
+	writel(0x2800, 0x7f008028);
+	writel(0x2802, 0x7f00802c);
+
+	writel(0x11112222, 0x7f008040); /* gpc */
+	writel(0x0d, 0x7f008044);
+
+	writel(0x00044474, 0x7f008060); /* gpd */
+	writel(0x03, 0x7f008064);
+	writel(0x02aa, 0x7f00806c);
+
+	writel(0x00011111, 0x7f008080); /* gpe */
+
+	writel(0xb6aaaa6a, 0x7f0080a0); /* gpf */
+	writel(0x6ffe, 0x7f0080a4);
+	writel(0x08000000, 0x7f0080a8);
+	writel(0x2aaaaaaa, 0x7f0080ac);
+
+	writel(0x01222222, 0x7f0080c0); /* gpg */
+
+	writel(0x11222222, 0x7f0080e0); /* gph */
+	writel(0x00000011, 0x7f0080e4);
+	writel(0x3e, 0x7f0080e8);
+
+	writel(0xaaaaaaaa, 0x7f008100); /* gpi */
+
+	writel(0x00aaaaaa, 0x7f008120); /* gpj */
+
+	writel(0x11111111, 0x7f008800); /* gpk */
+	writel(0x11100000, 0x7f008804);
+	writel(0x00001f80, 0x7f008808);
+	writel(0x00008000, 0x7f00880c);
+
+	writel(0x11100011, 0x7f008810); /* gpl */
+	writel(0x00100100, 0x7f008814);
+	writel(0x00004318, 0x7f008818);
+	writel(0x05, 0x7f00881c);
+
+	writel(0x00111111, 0x7f008820); /* gpm */
+
+	writel(0x20a00006, 0x7f008830); /* gpn */
+	writel(0x0000cdfb, 0x7f008834);
+	writel(0x00000006, 0x7f008838);
+
+	writel(0x55555555, 0x7f008140); /* gpo */
+	writel(0x00008000, 0x7f008144);
+
+	writel(0x1555aaa5, 0x7f008160); /* gpp */
+	writel(0x000000e4, 0x7f008164);
+
+	writel(0x00015555, 0x7f008180); /* gpq */
+
+	writel(0xbfcd1501, 0x7f0081a0); /* spcon */
+
+	writel(0x00, 0x7c100000); /* USB OTG */
+	writel(0x20, 0x7c100004);
+	writel(0x00, 0x7c100008);
 
 	return 0;
 }
